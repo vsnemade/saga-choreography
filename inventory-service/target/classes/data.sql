801893cf73fbb6295d127043c4bc7d8410f18665
Vishal Nemade <vsnemade@gmail.com>
2025-06-28 16:49:37 +0530

    Removed target

--- inventory-service/target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS order_inventory;
-CREATE TABLE order_inventory AS SELECT * FROM CSVREAD('classpath:order_inventory.csv');
--- payment-service/target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS user_balance;
-CREATE TABLE user_balance AS SELECT * FROM CSVREAD('classpath:user_balance.csv');
